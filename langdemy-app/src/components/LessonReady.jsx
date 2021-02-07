04b946e15247a770d1790c231c666fe99f3ca4b3
tanakayuu <tanaka07yuu23@gmail.com>
2021-02-08 00:26:01 +0900

    Add: React-Routerの導入

---
+++ langdemy-app/src/components/LessonReady.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class LessonReady extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        <h1>LessonReady</h1>
+      </div>
+    );
+  }
+}
+
+export default LessonReady;
---
+++ langdemy-app/src/components/Register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Register extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+}
+
+export default Register;
