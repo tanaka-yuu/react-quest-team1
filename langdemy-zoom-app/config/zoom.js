ad509e044cc3277b1c6cb9816eee4decb85adea1
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2021-02-12 23:54:22 +0900

    refectoring zoom.js

--- langdemy-zoom-app/config/zoom.js
+++ langdemy-zoom-app/config/zoom.js
@@ -5,9 +5,19 @@ const TOKEN_URL =
   "https://zoom.us/oauth/token?grant_type=authorization_code&code=";
 const USER_URL = "https://api.zoom.us/v2/users/me";
 
+const tokenURL = ZOOM.TOKEN_URL + req.query.code;
+const tokenRedirectURI = ZOOM.REDIRECT_URI +process.env.redirectURL;
+const POST_URL = tokenURL + tokenRedirectURI;
+
+const oauthURL = ZOOM.URL_OAUTH + process.env.clientID;
+const redirectURI = ZOOM.REDIRECT_URI + process.env.redirectURL;
+const REDIRECT_URL = oauthURL + redirectURI;
+
 module.exports = {
   URL_OAUTH,
   REDIRECT_URI,
   TOKEN_URL,
   USER_URL,
+  POST_URL,
+  REDIRECT_URL,
 };
