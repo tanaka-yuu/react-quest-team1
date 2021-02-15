d682ad9cd5c00101c6fc36b13c61b951bc6af44d
volcite <volcite.yuki.takano@gmail.com>
2021-02-15 23:48:05 +0900

    fix:不要ファイル削除

--- langdemy-zoom-app/views/jsonResponse.js
+++
@@ -1,8 +0,0 @@
-module.exports = (getBodyParam) => {
-<<<<<<< HEAD
-    return '<pre><code>' + JSON.stringify(getBodyParam, null, 2) + '</code></pre>';
-  };
-=======
-  return '<pre><code>' + JSON.stringify(getBodyParam, null, 2) + '</code></pre>';
-};
->>>>>>> 4d0c87575a182855568bee4a5d2e63e2c158aa58
