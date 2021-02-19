83c12c0fffc55476b4a1cbb5299d5d22f8e19a22
volcite <volcite.yuki.takano@gmail.com>
2021-02-20 05:36:52 +0900

    fix:コメント追加

--- langdemy-app/src/reducks/schedule/controller.js
+++ langdemy-app/src/reducks/schedule/controller.js
@@ -3,25 +3,27 @@ import firebase from "firebase/app"
 import axios from 'axios';
 import { takeEvery, fork, call, put } from 'redux-saga/effects';
 
-
 function* callData(appointment){
-
   console.log('---------------');
+  //ログインユーザ取得
+  const user = yield firebase.auth().currentUser;
+  //Expressにアクセス
+  return axios({
+    methiod: 'get',
+    url: `http://localhost:4000/createMeeting/${appointment.startTime}:00Z/${appointment.classTime}`,
+  })
+  .then((result) => {
+    //ExpressからZoomのURLが返ってくる
+    const joinUrl = result;
+    //ランダムなレッスンIDを作成
+    const lessonId = Math.random().toString(32).substring(2);
 
-    const user = yield firebase.auth().currentUser;
+    const startTime = new Date(appointment.startTime)
+    const finishTime = new Date(appointment.startTime)
+    //終了時間設定
+    finishTime.setMinutes(finishTime.getMinutes() + appointment.classTime);
 
-    return axios({
-      methiod: 'get',
-      url: `http://localhost:4000/createMeeting/${appointment.startTime}:00Z/${appointment.classTime}`,
-    })
-    .then((result) => {
-      const joinUrl = result;
-      const lessonId = Math.random().toString(32).substring(2);
-
-      const startTime = new Date(appointment.startTime)
-      const finishTime = new Date(appointment.startTime)
-      finishTime.setMinutes(finishTime.getMinutes() + appointment.classTime);
-
+    //DBに保存するデータ作成
     const lessonData = {
       lessonId: lessonId,
       startTime: firebase.firestore.Timestamp.fromDate(startTime),
